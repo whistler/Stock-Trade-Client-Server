f1363089908d2a49cfedfcceae5a25d76054574a
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2013-05-28 22:49:01 -0700

    created a new maven project

---
+++ src/main/java/com/ubc/cics525/javarmi/App.java
@@ -0,0 +1,13 @@
+package com.ubc.cics525.javarmi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
