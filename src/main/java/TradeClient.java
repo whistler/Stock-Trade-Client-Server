3d2cb6513c29861417e4cb9cfc3c6f55298d7a97
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2013-05-31 09:27:15 -0700

    Java RMI registry

--- src/main/java/TradeClient.java
+++ src/main/java/TradeClient.java
@@ -1,14 +1,26 @@
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class TradeClient {
 
 	private String clientname ;
-   	private static String host = "localhost";
-    	private static int portnumber = 51234;
+   	private static final String REGISTRYHOST = "localhost";
+    private static int REGISTRYPORT = 1099;
+    private static final String SERVERPATH = "rmi://localhost/myserver";
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			Registry registry = LocateRegistry.getRegistry(REGISTRYHOST, REGISTRYPORT);
+			System.setSecurityManager(new RMISecurityManager());
+			TradeServer server = (TradeServer) Naming.lookup(SERVERPATH);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
--- src/main/java/TradeServer.java
+++ src/main/java/TradeServer.java
@@ -1,4 +1,12 @@
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.RMIClientSocketFactory;
+import java.rmi.server.RMIServerSocketFactory;
+import java.rmi.server.UnicastRemoteObject;
 import java.sql.SQLException;
+
+import api.TradeApi;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
@@ -6,15 +14,24 @@ import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 
 
-public class TradeServer {
-
+public class TradeServer extends UnicastRemoteObject implements TradeApi {
+	private static final int PORT = 1099;
+	private static final String REGISTRYNAME = "tradeserver";
+	private static final String DATASOURCE = "jdbc:sqlite:stock_trade.db";
+	private static Registry registry;
 	/**
 	 * @param args
 	 * @throws SQLException 
 	 */
+	
+	public TradeServer() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 	public static void main(String[] args) throws SQLException {
 		ConnectionSource connectionSource = 
-				new JdbcConnectionSource("jdbc:sqlite:stock_trade.db");
+				new JdbcConnectionSource(DATASOURCE);
 	 	
 		Dao<Stock, String> stockDao =
 		  DaoManager.createDao(connectionSource, Stock.class);
@@ -26,24 +43,31 @@ public class TradeServer {
 		user.setUsername("Ibrahim");
 		userDao.createIfNotExists(user);
 		
+		try{
+			//Registry registry = java.rmi.registry.LocateRegistry.createRegistry(PORT);
+			registry = LocateRegistry.createRegistry(PORT);
+			registry.bind(REGISTRYNAME, new TradeServer());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 	
-	public float query(String ticker_name)
+	public float query(String ticker_name) throws RemoteException
 	{
 		return 0;
 	}
 
-	public String buy(String ticker_name, int num_stocks)
+	public String buy(String ticker_name, int num_stocks) throws RemoteException
+	{
+		return "SUCCESS";
+	}
+
+	public String sell(String ticker_name, int num_stocks) throws RemoteException
 	{
 		return "SUCCESS";
 	}
 	
-	public String sell(String ticker_name, int num_stocks)
-	{
-		return "SUCCESS";
-	}
-	
-	public String update(String ticker_name, float price)
+	public String update(String ticker_name, float price) throws RemoteException
 	{
 		return "SUCCESS";
 	}
