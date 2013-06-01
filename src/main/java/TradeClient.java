675375f129ccf73c81e19ff0cc4f02c3e8ae7608
Jaspreet Thind <jayzthind@gmail.com>
2013-06-01 10:59:57 -0700

    Working on client

--- src/main/java/TradeClient.java
+++ src/main/java/TradeClient.java
@@ -2,26 +2,57 @@ import java.rmi.Naming;
 import java.rmi.RMISecurityManager;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 
 import api.TradeApi;
 
 public class TradeClient {
 
-	private String clientname ;
+	private static String clientname ;
    	private static final String REGISTRYHOST = "localhost";
     private static int REGISTRYPORT = 1099;
     private static final String SERVERPATH = "rmi://localhost/tradeserver";
+    private static Scanner sc;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("TRADE CLIENT");
+		System.out.println("Please enter your username:");
+		sc = new Scanner(System.in);
+		
+		getUsernameEntry();
+
+		System.out.println("Hello " + clientname);
+
+		printOptions();
+		
 		try {
 			Registry registry = LocateRegistry.getRegistry(REGISTRYHOST, REGISTRYPORT);
 			TradeApi server = (TradeApi) Naming.lookup(SERVERPATH);
 			System.out.println(server.query("Foo"));
 		} catch (Exception ex) {
 			ex.printStackTrace();
+		}
+		
+		sc.close();
+	}
+	
+	private static void printOptions() {
+		System.out.println("----------------------------------------------------------------");
+		System.out.println("| Available options: ");
+		System.out.println("|\t query ");
+		System.out.println("|\t buy ");
+		System.out.println("|\t sell ");
+		System.out.println("----------------------------------------------------------------");
+	}
+	
+	private static void getUsernameEntry() {
+		if(sc.hasNext())
+		{
+			clientname = sc.next();
+			System.out.println(clientname);
 		}
 	}
 
