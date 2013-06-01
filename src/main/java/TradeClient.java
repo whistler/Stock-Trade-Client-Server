2e3249a6028734662298f27e7cfc68afd62f0d06
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2013-06-01 10:51:45 -0700

    Stock updater can fetch data from server

--- src/main/java/TradeClient.java
+++ src/main/java/TradeClient.java
@@ -7,7 +7,6 @@ import api.TradeApi;
 
 public class TradeClient {
 
-	private String clientname ;
    	private static final String REGISTRYHOST = "localhost";
     private static int REGISTRYPORT = 1099;
     private static final String SERVERPATH = "rmi://localhost/tradeserver";
@@ -15,9 +14,8 @@ public class TradeClient {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		try {
-			Registry registry = LocateRegistry.getRegistry(REGISTRYHOST, REGISTRYPORT);
+			//Registry registry = LocateRegistry.getRegistry(REGISTRYHOST, REGISTRYPORT);
 			TradeApi server = (TradeApi) Naming.lookup(SERVERPATH);
 			System.out.println(server.query("Foo"));
 		} catch (Exception ex) {
